8e747d590fbae8132943a482311fc04a359afd77
Pedenite <pedenite@gmail.com>
2020-09-09 18:44:12 -0300

    Removing unneccessary functions

--- classes/ppm.py
+++ classes/ppm.py
@@ -1,5 +1,6 @@
 class PPM:
     id = 'P3'
+    pixels = []
     def __init__(self, size1, size2, comp):
         self.size1 = size1
         self.size2 = size2
@@ -9,3 +10,4 @@ class PPM:
         print(self.id)
         print(self.size1, self.size2)
         print(self.comp)
+        #print(self.pixels)
--- icfms.py
+++ icfms.py
@@ -14,57 +14,68 @@ def prepare_PPM():
     c = file.readline()
 
     test = PPM(abc[0], abc[1], int(c))
-    test.show()
-
-    return file, test
-
-def process_PPM(file, test):
-    new_file = open(".tempICFMS/result.ppm", "w")
-    new_file.write(test.id+'\n')
-    new_file.write(str(test.size1)+' '+str(test.size2)+'\n')
-    new_file.write(str(test.comp)+'\n')
-
-    i = 0
-    while(True):
+    d = ""
+    while True:
         d = file.readline()
-        if(d == ''):
+        if d == '':
             break
         bcd = [int(n) for n in d.split()]
-        #bcd = [i+100 for i in bcd]
-        i+=1
-        print(i)
-        #change_PPM(bcd)
-        #crop_PPM(bcd, test)
-        
-        bcd = str(bcd).replace('[', '')
-        bcd = bcd.replace(']', '')
-        new_file.write(bcd.replace(',', '')+'\n')
+        test.pixels.extend(bcd)
 
     file.close()
+    test.show()
+
+    return test
+
+def crop_PPM(ppm):
+    i = j = 0
+    m_x = m_y = 0
+    crop_x = crop_y = 0
+    limit = 10
+    while i < ppm.size2:
+        j = 0
+        while j < ppm.size1*3:
+            if ppm.pixels[j + ppm.size1*3*i] > 240:
+                crop_x += 1
+            else:
+                crop_x = 0
+            
+            if crop_x >= limit*3:
+                m_x += j - limit*3
+                break
+            j += 1
+
+        if j-limit*3 <= 5:
+            crop_y += 1
+        else:
+            crop_y = 0
+
+        if crop_y >= limit:
+            m_y = i
+            break
+        i += 1
+    
+    # m_x = m_x/i
+
+    print(m_x, m_y, i, j)
+
+def save(file_name, ppm):
+    new_file = open(".tempICFMS/result.ppm", "w")
+    new_file.write(ppm.id+'\n')
+    new_file.write(str(ppm.size1)+' '+str(ppm.size2)+'\n')
+    new_file.write(str(ppm.comp)+'\n')
+    new_file.write(str(ppm.pixels).replace('[', '').replace(']', '').replace(',', '')+'\n')
     new_file.close()
 
-def change_PPM(rgb):
-    i = 0
-    while(i < len(rgb)):
-        rgb[i+2]=0
-        i+=3
-
-def crop_PPM(rgb, ppm):
-    i = 0
-    cont = 100
-    while(i < len(rgb)):
-        print()
-
-def save(file_name):
     temp = file_name.split(".")
-    if(len(temp) == 1):
+    if len(temp) == 1:
         file_name = temp[0] + ".ppm"
         print(file_name)
 
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
     output_file = open(file_name, "w")
 
-    if(temp[len(temp)-1] == "jpg" or temp[len(temp)-1] == "png" or temp[len(temp)-1] == "jpeg"):
+    if temp[len(temp)-1] == "jpg" or temp[len(temp)-1] == "png" or temp[len(temp)-1] == "jpeg":
         os.system("convert .tempICFMS/result.ppm " + file_name)
     else:
         os.system("mv .tempICFMS/result.ppm " + file_name)
@@ -77,8 +88,8 @@ def image_processing():
         return
     for i,file_name in enumerate(fileList):
         get_file(file_name)
-        file, ppm = prepare_PPM()
-        process_PPM(file, ppm)
+        ppm = prepare_PPM()
+        crop_PPM(ppm)
 
         dirs = file_name.split('/')
         dirs.pop()
@@ -86,7 +97,7 @@ def image_processing():
         if len(dirs) > 0:
             direc = str(dirs).replace('[', '').replace(']', '').replace(',', '/').replace('\'', '') # get directory from file_name
 
-        save(direc + "/result/out" + str(i) + "." + str(file_name.split('.').pop()))  
+        save(direc + "/result/out" + str(i) + "." + str(file_name.split('.').pop()), ppm)  
 
 if(__name__ == "__main__"):
     image_processing()
